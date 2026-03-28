96b41b9a89479c398f5e2db47a9628072b024615
Pratham Vaidya <iamprathamvaidya@gmail.com>
2026-03-28 17:47:29 +0530

    chore(lint): added lint rules

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
-import "./App.css";
+import { useState } from "react";
+import "./app.css";
 
 function App() {
-  return (
-    <>
-      <h1>Never Stop Building</h1>
-    </>
-  );
+  const [state, setState] = useState(0); // ❌ Hook in condition
+  setState(state + 1);
+  console.log(state);
+
+  return <h1>Never Stop Building</h1>;
 }
 
 export default App;
